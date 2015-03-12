323b518cc9ab004516cf29ee023ddbbdd7810b86
Szymon <Majewski>
2015-03-12 02:37:13 +0100

    kompletny skrypt FppCamera

--- Behaviours/FppCamera.cpp
+++ Behaviours/FppCamera.cpp
@@ -7,56 +7,22 @@
 #include "../GraphicsLog.h"
 
 FppCamera::FppCamera(GameObject* gameObject, const std::string& name) :
-	Behaviour(gameObject, name)
+	Behaviour(gameObject, name),
+	m_moveSensitivity(1.0f),
+	m_rotateSensitivity(1.0f),
+	m_angleX(0.0f),
+	m_angleY(0.0f),
+	m_rotating(0.0f),
+	m_lastCursorPos(0, 0)
 {
+	RegisterParameter("move_sensitivity", &m_moveSensitivity);
+	RegisterParameter("rotate_sensitivity", &m_rotateSensitivity);
 }
 
 void FppCamera::Update()
 {
-	sm::Vec3 position = m_gameObject->Transform.GetPosition();
-	static float angleY = 0.0f;
-	static float angleX = 0.0f;
-
-	float moveSpeed = 1.0f;
-
-	if (Input::GetKey(KeyCode_W))
-		position += m_gameObject->Transform.GetForward() * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_S))
-		position -= m_gameObject->Transform.GetForward() * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_A))
-		position -= m_gameObject->Transform.GetRight() * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_D))
-		position += m_gameObject->Transform.GetRight() * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_Q))
-		position += sm::Vec3(0, 1, 0) * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_E))
-		position -= sm::Vec3(0, 1, 0) * moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_Left))
-		angleY -= moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_Right))
-		angleY += moveSpeed * Time::DeltaTime;
-
-	if (Input::GetKey(KeyCode_Up))
-		angleX += moveSpeed * Time::DeltaTime;
-	if (Input::GetKey(KeyCode_Down))
-		angleX -= moveSpeed * Time::DeltaTime;
-
-	m_gameObject->Transform.SetPosition(position);
-
-	sm::Quat rotation =
-		sm::Quat::FromAngleAxis(angleY, sm::Vec3(0, 1, 0)) *
-		sm::Quat::FromAngleAxis(angleX, sm::Vec3(1, 0, 0));
-
-	sm::Vec3 forward = rotation.Rotate(sm::Vec3(0, 0, 1));
-
-	m_gameObject->Transform.SetForward(forward);
+	UpdatePosition();
+	UpdateRotation();
 
 	/*
 	GraphicsLog::AddSegment(position, position + forward);
@@ -64,4 +30,71 @@ void FppCamera::Update()
 	GraphicsLog::AddSegment(position, position + m_gameObject->Transform.GetUp(), sm::Vec3(0, 1, 0));
 	GraphicsLog::AddSegment(position, position + m_gameObject->Transform.GetRight(), sm::Vec3(1, 0, 0));
 	*/
+}
+
+void FppCamera::UpdatePosition()
+{
+	sm::Vec3 position = m_gameObject->Transform.GetPosition();
+
+	if (Input::GetKey(KeyCode_W))
+		position -= m_gameObject->Transform.GetForward() * m_moveSensitivity * Time::DeltaTime;
+
+	if (Input::GetKey(KeyCode_S))
+		position += m_gameObject->Transform.GetForward() * m_moveSensitivity * Time::DeltaTime;
+
+	if (Input::GetKey(KeyCode_A))
+		position -= m_gameObject->Transform.GetRight() * m_moveSensitivity * Time::DeltaTime;
+
+	if (Input::GetKey(KeyCode_D))
+		position += m_gameObject->Transform.GetRight() * m_moveSensitivity * Time::DeltaTime;
+
+	if (Input::GetKey(KeyCode_Q))
+		position += sm::Vec3(0, 1, 0) * m_moveSensitivity * Time::DeltaTime;
+
+	if (Input::GetKey(KeyCode_E))
+		position -= sm::Vec3(0, 1, 0) * m_moveSensitivity * Time::DeltaTime;
+
+	m_gameObject->Transform.SetPosition(position);
+}
+
+void FppCamera::UpdateRotation()
+{
+	// just clicked
+	if (Input::GetKey(KeyCode_Mouse0) && !m_rotating)
+	{
+		m_rotating = true;
+		m_lastCursorPos = Input::GetMousePosition();
+		return;
+	}
+
+	// just released
+	if (!Input::GetKey(KeyCode_Mouse0) && m_rotating)
+	{
+		m_rotating = false;
+		return;
+	}
+
+	// during rotation
+	if (Input::GetKey(KeyCode_Mouse0) && m_rotating)
+	{
+		sm::Point<int> currentMousePos = Input::GetMousePosition();
+		if (currentMousePos == m_lastCursorPos)
+			return;
+
+		m_angleY += (float)(currentMousePos.X - m_lastCursorPos.X) * m_rotateSensitivity;
+		m_angleX += (float)(currentMousePos.Y - m_lastCursorPos.Y) * m_rotateSensitivity;
+
+		m_lastCursorPos = Input::GetMousePosition();
+	}
+
+	if (!m_rotating)
+		return;
+
+	sm::Quat rotation =
+		sm::Quat::FromAngleAxis(m_angleY, sm::Vec3(0, 1, 0)) *
+		sm::Quat::FromAngleAxis(m_angleX, sm::Vec3(1, 0, 0));
+
+	sm::Vec3 forward = rotation.Rotate(sm::Vec3(0, 0, 1));
+
+	m_gameObject->Transform.SetForward(forward);
 }
--- Behaviours/FppCamera.h
+++ Behaviours/FppCamera.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Behaviour.h"
+#include <Graphics/Point.h>
 #include <string>
 
 class GameObject;
@@ -11,4 +12,16 @@ public:
 	FppCamera(GameObject* gameObject, const std::string& name);
 
 	void Update();
+
+private:
+	float m_moveSensitivity;
+	float m_rotateSensitivity;
+
+	float m_angleX;
+	float m_angleY;
+	bool m_rotating;
+	sm::Point<int> m_lastCursorPos;
+
+	void UpdatePosition();
+	void UpdateRotation();
 };
