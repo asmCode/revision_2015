a506498335507d5fcf25bf9840c99b16f07e089e
Szymon <Majewski>
2015-03-22 19:34:00 +0100

    enum LayerId

---
+++ FuturisEngine/LayerId.h
@@ -0,0 +1,38 @@
+#pragma once
+
+enum LayerId
+{
+	LayerId_0 = 1 << 0,
+	LayerId_1 = 1 << 1,
+	LayerId_2 = 1 << 2,
+	LayerId_3 = 1 << 3,
+	LayerId_4 = 1 << 4,
+	LayerId_5 = 1 << 5,
+	LayerId_6 = 1 << 6,
+	LayerId_7 = 1 << 7,
+	LayerId_8 = 1 << 8,
+	LayerId_9 = 1 << 9,
+	LayerId_10 = 1 << 10,
+	LayerId_11 = 1 << 11,
+	LayerId_12 = 1 << 12,
+	LayerId_13 = 1 << 13,
+	LayerId_14 = 1 << 14,
+	LayerId_15 = 1 << 15,
+	LayerId_16 = 1 << 16,
+	LayerId_17 = 1 << 17,
+	LayerId_18 = 1 << 18,
+	LayerId_19 = 1 << 19,
+	LayerId_20 = 1 << 20,
+	LayerId_21 = 1 << 21,
+	LayerId_22 = 1 << 22,
+	LayerId_23 = 1 << 23,
+	LayerId_24 = 1 << 24,
+	LayerId_25 = 1 << 25,
+	LayerId_26 = 1 << 26,
+	LayerId_27 = 1 << 27,
+	LayerId_28 = 1 << 28,
+	LayerId_29 = 1 << 29,
+	LayerId_30 = 1 << 30,
+	LayerId_31 = 1 << 31
+};
+
