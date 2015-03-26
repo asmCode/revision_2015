4881da09bd11ddebe6fb7fc25de60c3950c160ea
Szymon <Majewski>
2015-03-27 00:32:16 +0100

    klasa obslugujaca vbo

---
+++ FuturisEngine/Graphics/VertexBufferVec3.cpp
@@ -0,0 +1,82 @@
+#include "VertexBufferVec3.h"
+#include <GL/glew.h>
+#include <stddef.h>
+#include <memory>
+
+namespace FuturisEngine
+{
+	namespace Graphics
+	{
+		VertexBufferVec3::VertexBufferVec3() :
+			m_vboId(0),
+			m_count(0),
+			m_data(NULL)
+		{
+		}
+
+		VertexBufferVec3::~VertexBufferVec3()
+		{
+			if (m_vboId != 0)
+				glDeleteBuffers(1, &m_vboId);
+
+			DeleteData();
+		}
+
+		void VertexBufferVec3::Initialize()
+		{
+			glGenBuffers(1, &m_vboId);
+		}
+
+		void VertexBufferVec3::Setup(int channel)
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, m_vboId);
+			glVertexAttribPointer(channel, 3, GL_FLOAT, false, 0, 0);
+		}
+
+		void VertexBufferVec3::Apply(bool keepReadable)
+		{
+			assert(m_vboId != 0);
+			assert(m_data != NULL);
+
+			glBindBuffer(GL_ARRAY_BUFFER, m_vboId);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(sm::Vec3) * m_count, m_data, GL_STATIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			if (!keepReadable)
+				DeleteData();
+		}
+
+		int VertexBufferVec3::GetCount() const
+		{
+			return m_count;
+		}
+
+		void VertexBufferVec3::SetData(const sm::Vec3* data, int count)
+		{
+			if (m_data == NULL || m_count != count)
+			{
+				DeleteData();
+
+				m_count = count;
+				m_data = new sm::Vec3[m_count];
+			}
+
+			memcpy(m_data, data, m_count * sizeof(sm::Vec3));
+		}
+
+		const sm::Vec3* VertexBufferVec3::GetData() const
+		{
+			return m_data;
+		}
+
+		void VertexBufferVec3::DeleteData()
+		{
+			if (m_data != NULL)
+			{
+				delete[] m_data;
+				m_data = NULL;
+			}
+		}
+	}
+}
+
---
+++ FuturisEngine/Graphics/VertexBufferVec3.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Math/Vec3.h>
+#include <stdint.h>
+
+namespace FuturisEngine
+{
+	namespace Graphics
+	{
+		class VertexBufferVec3
+		{
+		public:
+			VertexBufferVec3();
+			~VertexBufferVec3();
+
+			void Initialize();
+
+			void Setup(int channel = 0);
+			void Apply(bool keepReadable = false);
+
+			int GetCount() const;
+			void SetData(const sm::Vec3* data, int count);
+			const sm::Vec3* GetData() const;
+
+		private:
+			uint32_t m_vboId;
+			int m_count;
+			sm::Vec3* m_data;
+
+			void DeleteData();
+		};
+	}
+}
+
