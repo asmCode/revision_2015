6b05d576bb52174077b1d9dd61e0121fb3b85ebd
Szymon <Majewski>
2015-08-12 23:26:06 +0200

    cleaning

--- SynchEventsLoader.cpp
+++ SynchEventsLoader.cpp
@@ -34,55 +34,28 @@ bool SynchEventsLoader::LoadFromFile(const std::string& path, SynchManager* sync
 bool SynchEventsLoader::Generate(SynchManager* synchManager)
 {
 
-
-	float time = 0.0f;
-	/*
-	///////////
-	static const float BlinkBeatFirst = 11.34f;
-	static const float BlinkBeatLast = 32.0f;
-	static const float BlinkBeatCount = 15.0f * 2; // w kazdym z nich sa 4 beaty
-
-	static const float BlinkBeatLength = BlinkBeatLast - BlinkBeatFirst;
-	static const float BlinkBeatSingle = BlinkBeatLength / (BlinkBeatCount - 0.0f);
-
-	time = BlinkBeatFirst;
-	for (uint32_t i = 0; i < (uint32_t)BlinkBeatCount; i++)
-	{
-		synchManager->Addevent(new SynchEvent("blink1", time));
-		time += BlinkBeatSingle;
-	}
-	//////////
-	*/
-	static const float RollBeatFirst = 33.0f;
-	static const float RollBeatLast = 53.55f;
-	static const float RollBeatCount = 15.0f; // w kazdym z nich sa 4 beaty
-
-	static const float RollBeatLength = RollBeatLast - RollBeatFirst;
-	static const float RollBeatSingle = RollBeatLength / (RollBeatCount - 0.0f);
-
-	time = RollBeatFirst;
-	for (uint32_t i = 0; i < (uint32_t)RollBeatCount; i++)
-	{
-		synchManager->Addevent(new SynchEvent("beat1", time));
-		time += RollBeatSingle;
-	}
-	
-
-	///////////
-	static const float OpenMechArmBeatFirst = 54.88f;
-	static const float OpenMechArmBeatLast = 65.8f;
-	static const float OpenMechArmBeatCount = 8;
-
-	static const float OpenMechArmBeatLength = OpenMechArmBeatLast - OpenMechArmBeatFirst;
-	static const float OpenMechArmBeatSingle = OpenMechArmBeatLength / (OpenMechArmBeatCount - 0.0f);
-
-	time = OpenMechArmBeatFirst;
-	for (uint32_t i = 0; i < (uint32_t)OpenMechArmBeatCount; i++)
-	{
-		synchManager->Addevent(new SynchEvent("open_mech1", time));
-		time += OpenMechArmBeatSingle;
-	}
-	//////////
+	SetBunchEvents(synchManager, "blink1", 11.34f, 32.0, 30);
+	SetBunchEvents(synchManager, "beat1", 33.0f, 53.55f, 15);
+	SetBunchEvents(synchManager, "open_mech1", 54.88f, 65.8f, 8);
 
 	return true;
 }
+
+void SynchEventsLoader::SetBunchEvents(
+	SynchManager* synchManager,
+	const std::string& eventName,
+	float timeStart,
+	float timeEnd,
+	int count)
+{
+	float length = timeEnd - timeStart;
+	static const float OpenMechArmBeatSingle = length / (float)count;
+
+	float time = timeStart;
+	for (int i = 0; i < count; i++)
+	{
+		synchManager->Addevent(new SynchEvent(eventName, time));
+		time += OpenMechArmBeatSingle;
+	}
+}
+
--- SynchEventsLoader.h
+++ SynchEventsLoader.h
@@ -9,4 +9,11 @@ class SynchEventsLoader
 public:
 	static bool LoadFromFile(const std::string& path, SynchManager* synchManager);
 	static bool Generate(SynchManager* synchManager);
+
+	static void SetBunchEvents(
+		SynchManager* synchManager,
+		const std::string& eventName,
+		float timeStart,
+		float timeEnd,
+		int count);
 };
